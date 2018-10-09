e2426e2f2660882c643195bc10cfa45c33e81653
sandeepsatpathy08 <sandeepsatpathy08@gmail.com>
2018-10-09 09:09:34 +0530

    Added a new comment

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import TodoList from "./TodoList";
-  
+
+// Comment added
 var destination = document.querySelector("#container");
   
 ReactDOM.render(
